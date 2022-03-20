31edace30826846a9d812cac05f3b2694d90e5a4
Emiliano Haag <emilianohaag10@gmail.com>
2022-03-20 16:33:15 -0300

    Update compare-two-dates.py

--- compare-two-dates.py
+++ compare-two-dates.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 
-#returns a boolean value between two date values string type 
+#returns a boolean value between two date values (string-type) 
 #import this snippet in your code, then call this function
 
-#ex:
-#params date 1 ex: passedSevenDays("23/10/2022", "12/02/2021") => False
+# example:
+# passedSevenDays("23/10/2022", "12/02/2021") => False
 
 def passedSevenDays(dateA, dateB):
     dateA = datetime.strptime(v, "%d/%m/%Y")
