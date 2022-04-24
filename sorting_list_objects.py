4aa838b477f5866a9f71b4acb23c1930dbd62ce1
Emiliano Haag <emilianohaag10@gmail.com>
2022-04-24 18:36:41 -0300

    Sort a given list of objects
    Sort a given list of tuple objects with lambda expression

---
+++ sorting_list_objects.py
@@ -0,0 +1,14 @@
+items = [
+    ("prod1", 2),
+    ("prod2", 109),
+    ("prod3", 76),
+]
+
+
+# python doesn't know how to sort this list of objects, we have to define it.
+
+
+# use lambda as an anonnymous function for shorter and cleaner way
+items.sort(key=lambda item: item[1])
+
+print(items)
